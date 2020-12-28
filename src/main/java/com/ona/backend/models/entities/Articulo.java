5afa28e13552f5bcda174216fe54789e5bb0b538
jajcter <dmmanotoa@espe.edu.ec>
2020-12-28 01:17:30 -0500

    dev_manotoa
    Comentario

--- src/main/java/com/ona/backend/models/entities/Articulo.java
+++ src/main/java/com/ona/backend/models/entities/Articulo.java
@@ -27,7 +27,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Articulo implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	//dev_manotoa
 	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Basic(optional = false)
