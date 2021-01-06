c2558769c5033f8cbef85b4028d6e08c4a245889
jajcter <dmmanotoa@espe.edu.ec>
2021-01-06 02:38:26 -0500

    merge

--- Back_End_Proyecto/src/main/java/com/ona/backend/controllers/VentaController.java
+++ Back_End_Proyecto/src/main/java/com/ona/backend/controllers/VentaController.java
@@ -2,15 +2,22 @@ package com.ona.backend.controllers;
 
 import java.util.List;
 
+import javax.persistence.TemporalType;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Temporal;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
 
 import com.ona.backend.models.entities.Venta;
 import com.ona.backend.services.interfaces.IVentaService;
@@ -23,13 +30,21 @@ public class VentaController {
 	private IVentaService service;
 	
 	@GetMapping("/{id}")
-	public Venta retrieve(@PathVariable(value="id") Long id) {
-		return service.findById(id);
+	public ResponseUtility retrieve(@PathVariable(value="id") Long id) {
+		Venta vt = service.findById(id);
+		if(vt == null) {
+			return new ResponseUtility("Venta not found", HttpStatus.NOT_FOUND, null);
+		}
+		return new ResponseUtility("Venta found", HttpStatus.OK, vt);
 	}
 	
 	@GetMapping("")
-	public List<Venta> list() {
-		return service.findAll();
+	public ResponseUtility list() {
+		List<Venta> vt = service.findAll();
+		if(vt == null) {
+			return new ResponseUtility("Venta not found", HttpStatus.NOT_FOUND, null);
+		}
+		return new ResponseUtility("Venta found", HttpStatus.OK, vt);
 	}
 	
 	@PostMapping("")
@@ -37,6 +52,16 @@ public class VentaController {
 	public Venta create(@RequestBody Venta venta) {		
 		service.save(venta);
 		return venta;
+	}
+	
+	//-----------------------------------------------------------------//	
+	@GetMapping("/search_usuario/{id}")
+	public ResponseUtility findAllByUsuarioIdUsuario(@PathVariable Long id){
+		List<Venta> vt = service.findAllByUsuarioIdUsuario(id);
+		if(vt == null) {
+			return new ResponseUtility("Venta not found", HttpStatus.NOT_FOUND, null);
+		}
+		return new ResponseUtility("Venta found", HttpStatus.OK, vt);
 	}
 	
 }
