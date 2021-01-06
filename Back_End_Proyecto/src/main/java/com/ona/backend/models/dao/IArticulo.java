fcd4b0c7eabf9c12a6d0b49e883d4b87f5d82f8d
jajcter <dmmanotoa@espe.edu.ec>
2021-01-06 02:25:52 -0500

    Merge dev_ona and dev_manotoa

--- Back_End_Proyecto/src/main/java/com/ona/backend/models/dao/IArticulo.java
+++ Back_End_Proyecto/src/main/java/com/ona/backend/models/dao/IArticulo.java
@@ -1,9 +1,14 @@
 package com.ona.backend.models.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ona.backend.models.entities.Articulo;
 
-public interface IArticulo extends CrudRepository<Articulo, Long>{
+public interface IArticulo extends CrudRepository<Articulo, Long> {
 
+	@Query("SELECT c FROM Articulo c")
+	public List<Articulo> findByCategoriaId(Long id);
 }
